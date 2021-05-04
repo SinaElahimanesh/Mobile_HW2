a24f1652656fce9ee0042f69ef5c8d342a6fd8db
bardia-mhd <bardia.mohammadi@yahoo.com>
2021-05-04 20:13:28 +0430

    Bookmark fragment and layout

---
+++ app/src/main/java/edu/sharif/mobile_hw2/BookmarkAdapter.java
@@ -0,0 +1,91 @@
+package edu.sharif.mobile_hw2;
+
+import android.annotation.SuppressLint;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookmarkAdapter extends RecyclerView.Adapter<BookmarkAdapter.BookmarkViewHolder> implements Filterable{
+    private static final String TAG = "Filter";
+    private List<Bookmark> bookmarks;
+    private List<Bookmark> bookmarksFull;
+
+    public BookmarkAdapter(List<Bookmark> bookmarks) {
+        this.bookmarks = bookmarks;
+        bookmarksFull = new ArrayList<>(bookmarks);
+    }
+
+    @NonNull
+    @Override
+    public BookmarkViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.single_bookmark,parent,false);
+        return new BookmarkViewHolder(view);
+    }
+
+    @SuppressLint("DefaultLocale")
+    @Override
+    public void onBindViewHolder(@NonNull BookmarkViewHolder holder, int position) {
+        Bookmark bookmark = bookmarks.get(position);
+        holder.locationName.setText(bookmark.getName());
+        holder.locationLong.setText(String.format("%.3f",bookmark.getLatLong()));
+        holder.locationLat.setText(String.format("%.3f",bookmark.getLatLat()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return bookmarks.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return bookmarkFilter;
+    }
+
+    private Filter bookmarkFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence charSequence) {
+            List<Bookmark> filteredList = new ArrayList<>();
+            if(charSequence == null || charSequence.length() == 0)
+                filteredList.addAll(bookmarksFull);
+            else{
+                String pattern = charSequence.toString().trim().toLowerCase();
+                for (Bookmark bookmark : bookmarksFull) {
+                    if(bookmark.getName().trim().toLowerCase().contains(pattern))
+                        filteredList.add(bookmark);
+                }
+            }
+            FilterResults filterResults = new FilterResults();
+            filterResults.values = filteredList;
+            return filterResults;
+        }
+
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            bookmarks.clear();
+            bookmarks.addAll((List) filterResults.values);
+            notifyDataSetChanged();
+        }
+    };
+
+    class BookmarkViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView locationName, locationLat, locationLong;
+        public BookmarkViewHolder(@NonNull View itemView) {
+            super(itemView);
+            locationName = itemView.findViewById(R.id.bookmark_name);
+            locationLat = itemView.findViewById(R.id.bookmark_lat);
+            locationLong = itemView.findViewById(R.id.bookmark_long);
+        }
+    }
+}
--- app/src/main/java/edu/sharif/mobile_hw2/BookmarkFragment.java
+++ app/src/main/java/edu/sharif/mobile_hw2/BookmarkFragment.java
@@ -3,62 +3,77 @@ package edu.sharif.mobile_hw2;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BookmarkFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.arlib.floatingsearchview.FloatingSearchView;
+
 public class BookmarkFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private boolean firstFlag = true;
+    private RecyclerView bookmarksRecyclerView;
+    private ItemTouchHelper itemTouchHelper;
+    private FloatingSearchView floatingSearchView;
+    private BookmarkAdapter bookmarkAdapter;
 
     public BookmarkFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BookMarkFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BookmarkFragment newInstance(String param1, String param2) {
-        BookmarkFragment fragment = new BookmarkFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        firstFlag = !firstFlag;
+
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bookmark, container, false);
+        View view = inflater.inflate(R.layout.fragment_bookmark, container, false);
+
+        floatingSearchView = view.findViewById(R.id.search_bar_bookmarks);
+        if(Bookmark.getBookmarks().size() != 0){
+            TextView label = view.findViewById(R.id.no_bookmark_label);
+            label.setVisibility(View.INVISIBLE);
+            label.setEnabled(false);
+
+            bookmarksRecyclerView = view.findViewById(R.id.bookmarks_recyclerview);
+            itemTouchHelper.attachToRecyclerView(bookmarksRecyclerView);
+            bookmarkAdapter = new BookmarkAdapter(Bookmark.getBookmarks());
+            bookmarksRecyclerView.setLayoutManager(new LinearLayoutManager(view.getContext(),
+                    LinearLayoutManager.VERTICAL, false));
+            bookmarksRecyclerView.setAdapter(bookmarkAdapter);
+
+
+            if(!firstFlag)
+                Toast.makeText(view.getContext(),"Swipe left to delete",Toast.LENGTH_SHORT).show();
+
+            firstFlag = true;
+
+            floatingSearchView.setOnQueryChangeListener(new FloatingSearchView.OnQueryChangeListener() {
+                @Override
+                public void onSearchTextChanged(String oldQuery, String newQuery) {
+                    bookmarkAdapter.getFilter().filter(newQuery);
+                }
+            });
+
+        }else{
+            floatingSearchView.setVisibility(View.INVISIBLE);
+            floatingSearchView.setEnabled(false);
+        }
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
     }
 }
